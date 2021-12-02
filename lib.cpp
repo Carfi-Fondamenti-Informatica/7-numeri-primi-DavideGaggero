ccc7edeebd53cee44ef299aad640afac0174522a
DavideGaggero <91790589+DavideGaggero@users.noreply.github.com>
2021-12-02 07:30:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,7 +2,7 @@
 
 bool numeroPrimo(int a){
   bool primo = true;
-  for (int i=2; i<a; i++;){
+  for (int i=2; i<a; i++){
     int risultato = a % i;
     if (risultato==0 and i<a){
       primo = false;
