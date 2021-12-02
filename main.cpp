46803e01cb2f95d9821e7514681f5e6759338c7b
DavideGaggero <91790589+DavideGaggero@users.noreply.github.com>
2021-12-02 07:44:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,18 @@ int main (){
   int numero;
   cin >> numero;
   if (numero == 0){
-    cout << "numero primo"<< endl;
-  }eslse if( numero < 0) {
+    cout << "numero primo" << endl;
+  }else if( numero < 0) {
     return 0;
   }else{
-  
-  
-  if (numeroPrimo(numero)== true){
-    cout << "numero primo" << endl;
-  }else{
-    cout << "numero non primo" << endl;
-  }
-     }
-   return 0;
+    
+   
+    if (numeroPrimo(numero)== true){
+      cout << "numero primo" << endl;
+    }else{
+      cout << "numero non primo" << endl;
+    }
+         }
+     
+    return 0;
 }
