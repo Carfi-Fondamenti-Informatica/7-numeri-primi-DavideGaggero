8efb26dddcb1bae10ed6d04ecb7a888c6fc7fef7
DavideGaggero <91790589+DavideGaggero@users.noreply.github.com>
2021-12-02 02:15:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,9 +4,10 @@ bool numeroPrimo(int a){
   bool primo = true;
   for (int i=2; i<a; i++){
     int risultato = a % i;
-    if(risultato==0 and i<a){
+    if (risultato==0 and i<a){
       primo = false;
       break;
+   
     }
   }  
 return primo;
